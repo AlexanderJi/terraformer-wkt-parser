e388024574e55f86c4f23d00212cbd55f945393f
JianWeiJi <jwj930404@gmail.com>
2019-12-03 16:15:36 +0800

    Update terraformer-wkt-parser.d.ts

--- terraformer-wkt-parser.d.ts
+++ terraformer-wkt-parser.d.ts
@@ -1,5 +1,7 @@
 export = WKT;
 declare namespace WKT {
+    // @ts-ignore
     export function parse(wkt: string): GeoJSON.GeometryObject;
+    // @ts-ignore
     export function convert(geoJSON: GeoJSON.GeometryObject): string;
 }
